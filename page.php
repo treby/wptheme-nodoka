87fe5b32f4e84dc7af3f72fe55f0e1d2604b4e50
treby <treby@atelier-nodoka.net>
2014-01-06 00:11:26 +0900

    added my css.

--- page.php
+++ page.php
@@ -4,15 +4,27 @@
  *                      2011.10.20 treby
  */
  
-get_header(); ?>
+get_header();
+the_post();
+?>
 <div class="content">
   <div class="container">
-    <?php the_post(); ?>
-    <h2><?php the_title(); ?></h2>
-    <hr>
-
-    <div>
-      <?php the_content(); ?>
+    <div class="page">
+      <div class="row">
+        <div class="col-8 col-lg-8">
+          <article class="entry">
+            <header>
+              <h2><?php the_title(); ?></h2>
+            </header>
+            <section>
+              <?php the_content(); ?>
+            </section>
+          </article>
+        </div>
+        <div class="col-4 col-lg-4">
+          <?php get_sidebar(); ?>
+        </div>
+      </div>
     </div>
   </div>
 </div>
