71beb04c307640d5acc029445b4eb8e97f312f86
treby <treby@atelier-nodoka.net>
2014-05-11 23:12:36 +0900

    Adjust front-page design.

--- front-page.php
+++ front-page.php
@@ -3,29 +3,22 @@
  * front-page.php : front page
  *                         2013.10.14 treby
  */
- 
 get_header(); ?>
 <div class="content">
   <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <div id="da-slider" class="da-slider">
-          <div class="da-slide">
-            <div class="da-blue">
-              <img src="<?php echo get_template_directory_uri(); ?>/images/top.jpg" style="width:100%;">
-            </div>
-          </div>
+    <div id="da-slider" class="da-slider" style="margin-bottom: 1em;">
+      <div class="da-slide">
+        <div class="da-blue">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/top.jpg" style="width:100%;">
         </div>
       </div>
     </div>
     <div class="row">
-      <div class="container">
       <?php
         if ( function_exists('dynamic_sidebar')) {
             dynamic_sidebar('content_frontpage');
         }
       ?>
-      </div>
     </div>
   </div>
 </div>
