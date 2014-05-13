dc329257cad394c1d2b337ba05e0cc2406f21579
treby <treby@atelier-nodoka.net>
2014-05-14 00:08:23 +0900

    Update 404 page.

--- 404.php
+++ 404.php
@@ -1,18 +1,21 @@
-<?php 
+<?php
 /**
  * 404.php : 404 Template
- *                      2011.10.20 treby
+ *                      2014.05.14 treby
  */
- 
+
 get_header(); ?>
-<div class="jumbotron" id="the_content">
+<div class="content">
   <div class="container">
-    <h1>Oops!!</h1>
-    <p>"404 Page not found." is detected.</p>
+    <div class="row">
+      <div class="col-md-12 col-sm-12">
+        <div class="well c-soon">
+          <div class="soon-med">ページが見つかりませんでした。</div>
+          <div class="soon-big">404 <span class="color">NOT</span> FOUND.</div>
+          <p class="soon-small">もしかしたら、あなたがお探しのコンテンツ（特に古めのもの）は<br /><a href="http://classic.atelier-nodoka.net<?php echo $_SERVER['REQUEST_URI'] ?>">http://classic.atelier-nodoka.net<?php echo $_SERVER['REQUEST_URI']; ?></a>にあるかもしれません。</p>
+        </div>
+      </div>
+    </div>
   </div>
-</div>
-<div class="container">
-  <h2>ページが見つかりませんでした。</h2>
-  <p>もしかしたら、あなたがお探しのコンテンツ（特に古めのもの）は<a href="http://classic.atelier-nodoka.net<?php echo $_SERVER['REQUEST_URI'] ?>">http://classic.atelier-nodoka.net<?php echo $_SERVER['REQUEST_URI']; ?></a>にあるかもしれません（ないかもしれません）。</p>
 </div>
 <?php get_footer(); ?>
