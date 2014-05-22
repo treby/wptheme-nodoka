cfdaf3b3d1c768c00908b6db435ff30047534a21
treby <treby@atelier-nodoka.net>
2014-05-22 23:50:22 +0900

    Update year to 2014.

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@ date_default_timezone_set('Asia/Tokyo');
   <div class="container">
     <div class="copy text-right">
       <h6><?php bloginfo('name'); ?></h6>
-      <address>Copyright &copy; 2005-2013 Atelier NODOKA by treby(@treby006). <br />This site is powered by <a href="http://ja.wordpress.org/">WordPress</a> / <a href="http://getbootstrap.com/">Twitter Bootstrap v3.0</a> (<a href="https://wrapbootstrap.com/theme/metro-mania-no.1-metro-bootstrap-theme-WB07F9G8L">Metro Mania</a>)</address>
+      <address>Copyright &copy; 2005-2014 Atelier NODOKA by treby(@treby006). <br />This site is powered by <a href="http://ja.wordpress.org/">WordPress</a> / <a href="http://getbootstrap.com/">Twitter Bootstrap v3.0</a> (<a href="https://wrapbootstrap.com/theme/metro-mania-no.1-metro-bootstrap-theme-WB07F9G8L">Metro Mania</a>)</address>
       <hr>
       <p>Access: <?php echo counterize_getuniqueamount(); ?> (T: <?php echo counterize_getuniquehitstoday(); ?>, Y: <?php echo counterize_getuniquehitstheday(1) ?>, 7days: <?php echo counterize_getuniquelatest7days(); ?>)</p>
     </div>
